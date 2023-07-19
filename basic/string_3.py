ae67d0cd239a7fa005ce0efbf37f25bd21340b67
ngfusy7 <nguphusy7@gmail.com>
2023-07-19 09:11:03 +0700

    updated 19/7/2023

---
+++ basic/string_3.py
@@ -0,0 +1,21 @@
+# str = "Hello World!"
+
+# print(str.upper())
+# print(str.lower())
+# print(str.strip())
+
+# a = "Hello"
+# b = "World"
+# c = a + b
+# print(c)
+
+# age = 36
+# txt = "My name is John, and I am {}"
+# print(txt.format(age))
+
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
