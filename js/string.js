01e2655bb7a4a90861ddb8781d3e5de57b949502
ngfusy7 <nguphusy7@gmail.com>
2023-08-04 15:23:57 +0700

    updated 0408

--- js/string.js
+++ js/string.js
@@ -1,3 +1,9 @@
-let message = 'Hello!'; 
+// const fruit = ["Banana", "Orange", "Apple"];
+// console.log(fruit.sort());
 
-alert(message);
+let arr = [1,2,3,4,5];
+let sum = 0;
+for(let i = 0; i < 5; i++){
+    sum += arr[i];
+}
+console.log(sum);
