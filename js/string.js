e6996fc17218ba09158af31325abf1ed01c6145a
ngfusy7 <nguphusy7@gmail.com>
2023-08-05 15:15:21 +0700

    updated 5/8

--- js/string.js
+++ js/string.js
@@ -1,9 +1,23 @@
 // const fruit = ["Banana", "Orange", "Apple"];
 // console.log(fruit.sort());
 
-let arr = [1,2,3,4,5];
-let sum = 0;
-for(let i = 0; i < 5; i++){
-    sum += arr[i];
+// let arr = [1,2,3,4,5];
+// let sum = 0;
+// for(let i = 0; i < 5; i++){
+//     sum += arr[i];
+// }
+// console.log(sum);
+
+// var coures = {
+//     id: 1,
+//     name: 'Javascript',
+//     coin: 500
+// }
+// 
+// var array = ['a', 'b', 'c', 'a', 'b', 'c'];
+
+// console.log(new Set(array));
+
+function deQuy(){
+    deQuy();
 }
-console.log(sum);
