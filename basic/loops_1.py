7d28c1ce831a04b18dd59ee0b09e51903b385837
ngfusy7 <nguphusy7@gmail.com>
2023-09-07 16:58:19 +0700

    updated 7/9/23

---
+++ basic/loops_1.py
@@ -0,0 +1,17 @@
+# r = 5
+# for s in range(r):
+#     for c in range(s):
+#         print(' ', end="")
+#     print("#")
+    
+# height = 7
+# for s in range(height): # 0 -> 1 
+#     for c in range(height):
+#         print('*', end="") 
+#     height = height - 1 
+#     print("")
+
+height = 6 
+for s in range(height):
+    for c in range(height):
+        
