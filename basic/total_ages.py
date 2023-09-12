794bee2b66b886d6ac5bd5ec148de477ecfeacba
ngfusy7 <nguphusy7@gmail.com>
2023-09-12 11:02:02 +0700

    updated 12/9/23

---
+++ basic/total_ages.py
@@ -0,0 +1,14 @@
+
+def totalAge():
+    ageA = int(input("Enter A's age: "))
+    ageB = int(input("Enter B's age: "))
+    return ageA + ageB
+
+def main():
+    print(f"Total age is: ", totalAge())
+
+
+
+main()
+
+
