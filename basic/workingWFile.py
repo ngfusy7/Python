a9757c01c8857ba72cea366a51b6619032127766
ngfusy7 <nguphusy7@gmail.com>
2023-11-03 01:03:25 +0700

    updated 3/11/23

--- basic/workingWFile.py
+++ basic/workingWFile.py
@@ -1,11 +1,7 @@
-def main():
-    outfile = open('philosophes.txt', 'w')
-    
-    outfile.write('John Locke\n')
-    outfile.write('David Hume\n')
-    outfile.write('Edmund Burke\n')
-    
-    outfile.close()
-    
-if __name__ == '__main__':
-    main()
+# File Objects
+
+f = open('test.txt', 'r')
+
+print(f.mode)
+
+f.close()
